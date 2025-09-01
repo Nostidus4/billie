804e0bc421d5b117d09ab6b0e4e93f96a2725f38
Khang <nguyenhoangkhang10022005@gmail.com>
2025-09-01 16:53:56 +0700

    Creating Authentication APIs (Login, Sign-Up, Get User Info, Profile Image Upload), and test upload image

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path=require("path");
 const connectDB=require("./config/db.js")
+const authRoutes=require("./routes/authRoutes.js")
 
 const app = express();
 
@@ -14,10 +15,14 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.use((express.json()));
+app.use(express.json());
 
 connectDB();
+
+app.use("/api/v1/auth",authRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{console.log(`Server running on port ${PORT}`)});
