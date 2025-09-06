ec4a7a27505690b265dd47f643adc5637df4735e
Nostidus4 <xeongoctan@gmail.com>
2025-09-06 12:07:38 +0700

    Cập nhật authController và User model để dùng fullName khớp với main

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,8 +5,7 @@ const UserSchema = new mongoose.Schema(
   {
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    // profileImageUrl: { type: String, default: null } 
+    password: { type: String, required: true }
   },
   { timestamps: true }
 );
